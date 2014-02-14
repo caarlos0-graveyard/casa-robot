798da6bbe2cee9d228de0254c68c1ff7f7c24064
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-02-14 19:25:34 -0200

    quotes around the invalid command

--- api/src/main/java/com/robot/exception/InvalidRobotCommandException.java
+++ api/src/main/java/com/robot/exception/InvalidRobotCommandException.java
@@ -2,15 +2,15 @@ package com.robot.exception;
 
 /**
  * This exception is thrown when a invalid command is given to the Robot.
- * 
+ *
  * @author carlos
- * 
+ *
  */
 public class InvalidRobotCommandException extends RobotException {
 	private static final long serialVersionUID = 1L;
 
 	public InvalidRobotCommandException(char givenCommand) {
-		super("The command " + givenCommand
-				+ " is invalid. Must contain only 'M', 'L' and 'R' letters.");
+		super("The command '" + givenCommand
+				+ "' is invalid. Must contain only 'M', 'L' and 'R' letters.");
 	}
 }
