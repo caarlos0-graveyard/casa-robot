5f76c78b1b38502b8190eabbdac4329e54fcf8e0
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-03-17 22:36:37 -0300

    testing

--- api/src/test/java/com/robot/AllRobotTestSuite.java
+++ api/src/test/java/com/robot/AllRobotTestSuite.java
@@ -1,20 +1,14 @@
 package com.robot;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-import com.robot.tests.RobotPositionTest;
-import com.robot.tests.RobotTest;
-import com.robot.tests.WindRoseTest;
 
 /**
  * Suite of all Robot tests.
  * 
  * @author carlos
  */
-@RunWith(Suite.class)
-@Suite.SuiteClasses(value = { RobotTest.class, WindRoseTest.class,
-        RobotPositionTest.class })
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses(value = { RobotTest.class, WindRoseTest.class,
+//        RobotPositionTest.class })
 public class AllRobotTestSuite {
 
 }
